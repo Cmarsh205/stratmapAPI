ad0cda67ff413f27dba38f5aa6157a0f3da04d22
Cmarsh205 <156699625+Cmarsh205@users.noreply.github.com>
2025-11-11 13:33:49 -0330

    Jest setup
    -Added Jest for function testing

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+/** @type {import("ts-jest").JestConfig} **/
+export default {
+  preset: "ts-jest/presets/default-esm",
+  testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts"],
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        useESM: true,
+        tsconfig: "./tsconfig.json",
+      },
+    ],
+  },
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+};
